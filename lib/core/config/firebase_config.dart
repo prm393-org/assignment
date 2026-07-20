9abfc001865dbc61c758b7fa37991d4efd77a49a
Nguyễn Đức Huỳnh <kdshuynh48964@gmail.com>
2026-07-21 01:28:04 +0700

    feat: firebase config

---
+++ lib/core/config/firebase_config.dart
@@ -0,0 +1,9 @@
+/// Firebase / Google Sign-In constants for project `prm301-asm`.
+class FirebaseConfig {
+  FirebaseConfig._();
+
+  /// Web OAuth client ID from `google-services.json` (client_type: 3).
+  /// Required by [GoogleSignIn] on Android.
+  static const String googleWebClientId =
+      '833822181033-i7hntapp385deu3okl59phqhov1id010.apps.googleusercontent.com';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:chuoi_xanh_viet/app.dart';
+import 'package:chuoi_xanh_viet/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (details) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(details);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   runApp(const ProviderScope(child: ChuoiXanhVietApp()));
 }
